5cbeb94ce9aa2b13e746551f0f789d08a911e40e
scostavinicius <2003.costa.vinicius@gmail.com>
2025-03-16 23:43:39 -0300

    Lógica implementada

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+let listaAmigosHTML = document.getElementById("listaAmigos");
+let amigoSorteadoHTML = document.getElementById("resultado");
+
+const adicionarAmigo = () => {
+  let novoAmigo = document.getElementById("amigo").value.trim();
+
+  if (novoAmigo == "" || novoAmigo == undefined) {
+    alert("Por favor, insira um nome.");
+    return;
+  }
+
+  listaAmigos.push(novoAmigo);
+  console.log(novoAmigo);
+  console.log(listaAmigos);
+
+  document.getElementById("amigo").value = "";
+
+  atualizarListaHTML();
+};
+
+const atualizarListaHTML = () => {
+  listaAmigosHTML.innerHTML = "";
+  listaAmigos.forEach((amigo) => {
+    listaAmigosHTML.innerHTML += `<li>${amigo}</li>`;
+  });
+};
+
+const sortearAmigo = () => {
+  if (listaAmigos.length == 0) {
+    alert("Não é possível sortear: a lista está vazia.");
+  }
+
+  let indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+  let amigoSorteado = listaAmigos[indiceAleatorio];
+
+  amigoSorteadoHTML.innerHTML = `<li>${amigoSorteado}</li>`;
+
+  listaAmigos.splice(indiceAleatorio, 1);
+
+  console.log(amigoSorteado);
+  console.log(listaAmigos);
+};
